ccaa1f3cc8e939ef141dfac576c2cc3e40ae54cc
MohamedMiladi777 <miladyshady@gmail.com>
2025-04-04 07:30:09 +0000

    Fauture: Book can be downloaded from AWS S3 bucket

--- models/bookModel.js
+++ models/bookModel.js
@@ -3,40 +3,39 @@ import mongoose from "mongoose";
 
 // Define the Book schema
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String, // Field type is String
-        required: true,// The title is required when adding a book
-        //unique: true  The title must be unique
-    },
-    author: {
-        type: String, // Field type is String
-        required: true ,// The author is required when adding a book
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId, // Field type is String
-        ref: "Category", // Reference to the Category model
-        required: true // The category is required when adding a book
-    },
-    downloadUrl: {
-        type: String, // Field type is String
-        required: true // The downloadUrl is required when adding a book
-    },
-    description: {
-        type: String, // Field type is String
-        default: "No description available" // Default value if no description is provided
-    },
-    createdAt: {
-        type: Date, // Field type is Date
-        default: Date.now // Sets the current date/time by default
-    }
-    , fileId: {
-        type: String,
-        required: true
-    }
+  title: {
+    type: String, // Field type is String
+    required: true, // The title is required when adding a book
+    //unique: true  The title must be unique
+  },
+  author: {
+    type: String, // Field type is String
+    required: true, // The author is required when adding a book
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId, // Field type is String
+    ref: "Category", // Reference to the Category model
+    required: true, // The category is required when adding a book
+  },
+  downloadUrl: {
+    type: String, // Field type is String
+    required: true, // The downloadUrl is required when adding a book
+  },
+  description: {
+    type: String, // Field type is String
+    default: "No description available", // Default value if no description is provided
+  },
+  createdAt: {
+    type: Date, // Field type is Date
+    default: Date.now, // Sets the current date/time by default
+  },
+  fileId: {
+    type: String,
+    required: true,
+  },
 });
 
-
-bookSchema.index({category: 1}); // Index the category field Improves performance when searching books by category.
+bookSchema.index({ category: 1 }); // Index the category field Improves performance when searching books by category.
 
 // Export the Book model
 // module.exports = mongoose.model('Book', bookSchema);
